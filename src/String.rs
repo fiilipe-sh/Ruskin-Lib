e3adc7346200cd481f48e70a7c67bc162ad29afb
filipe constantino <constantinobundle@gmail.com>
2026-03-28 22:47:34 -0300

    Add files via upload

---
+++ src/String.rs
@@ -0,0 +1,21 @@
+use std::io::{self, Write};
+
+
+
+fn prompt_string(prompt: &str) -> String{
+
+    print!("{} ",prompt);
+
+    //forcing the user's text to appear on the screen
+    io::stdout().flush().expect("Error displaying your buffer.");
+
+    let mut input_user = String::new();
+
+    //Read the line that the user typed and saved in the variable.
+    io::stdin().read_line(&mut input_user).expect("Error in text input.");
+
+    //removing the enter key that remained in the buffer
+    let response = input_user.trim();
+
+    return response.to_string();
+}
---
+++ src/int.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Write};
+
+
+fn prompt_int32(prompt: i32) -> i32{
+
+
+
+    print!("{} ",prompt);
+
+    //forcing the user's text to appear on the screen
+    io::stdout().flush().expect("Error displaying your buffer.");
+
+    let mut input_user = String::new();
+
+    //Read the line that the user typed and saved in the variable.
+    io::stdin().read_line(&mut input_user).expect("Error in text input.");
+
+    //Check if it is an integer.
+    if let Err(error) = input_user.trim().parse::<i32>(){
+        println!("{}",error);
+
+    }
+
+    return input_user;
+}
+
+
+
+
+fn main(){
+    let number = prompt_int32("Digite sua idade: ");
+
+    println!("{}",number);
+
+
+
+
+}
