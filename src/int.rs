b9f7de8e65abee79004692a70807ffc6ca905c96
filipe constantino <constantinobundle@gmail.com>
2026-04-01 18:42:10 -0300

    Refactor prompt_int32 to accept string prompt

--- src/int.rs
+++ src/int.rs
@@ -1,25 +1,29 @@
 use std::io::{self, Write};
 
 
-fn prompt_int32(prompt: i32) -> i32{
+fn prompt_int32(prompt: &str) -> i32{
+
+     let mut input_user = String::new();
 
 
+      loop{
+          input_user.clear();
 
-    print!("{} ",prompt);
+          print!("{} ",prompt);
 
-    //forcing the user's text to appear on the screen
-    io::stdout().flush().expect("Error displaying your buffer.");
+          //forcing the user's text to appear on the screen
+          io::stdout().flush().expect("Error displaying your buffer.");
 
-    let mut input_user = String::new();
+          //Read the line that the user typed and saved in the variable.
+          io::stdin().read_line(&mut input_user).expect("Error in text input.");
 
-    //Read the line that the user typed and saved in the variable.
-    io::stdin().read_line(&mut input_user).expect("Error in text input.");
 
-    //Check if it is an integer.
-    if let Err(error) = input_user.trim().parse::<i32>(){
-        println!("{}",error);
+   match input_user.trim().parse::<i32>(){
 
-    }
-
-    return input_user;
+      Ok(n) => return n,
+      Err(_) => {
+           println!("{} is not a valid number! Try again.", input_user.trim());
+   }
+  }
+ };
 }
