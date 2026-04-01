9f2945ed356c1ccf40a06d416dd83c43ef9f6fbf
filipe constantino <constantinobundle@gmail.com>
2026-04-01 19:52:03 -0300

    Refactor prompt_u64 function for better user feedback

--- src/u64.rs
+++ src/u64.rs
@@ -1,27 +1,24 @@
 use std::io::{self, Write};
+const RESET: &str = "\x1b[0m";
+const RED: &str = "\x1b[31m";
+const GRAY: &str = "\x1b[90m";
 
 fn prompt_u64(prompt: &str) -> u64{
-
      let mut input_user = String::new();
-      loop{
+     loop{
           input_user.clear();
-
-          print!("{} ",prompt);
-
+          println!("{}[only positive numbers]{}",GRAY,RESET);
+          print!("{}:",prompt);
           //forcing the user's text to appear on the screen
           io::stdout().flush().expect("Error displaying your buffer.");
-
           //Read the line that the user typed and saved in the variable.
           io::stdin().read_line(&mut input_user).expect("Error in text input.");
 
    match input_user.trim().parse::<u64>(){
-     
       Ok(n) => return n,
       Err(_) => {
-           println!("{} is not a valid number! Try again.", input_user.trim());
-      }
+           println!("{}{} is not a valid number! Try again.{}",RED,input_user.trim(),RESET);
+     }
     }
   };
 }
-
-
